e80db06b4188a77237cf92f1838f57eeba85a24d
coden0w <coden0w@coden0w.io>
2025-11-23 23:06:38 +0100

    Corrections

--- Sources/AnimatedLauncherUI/AnimatedLauncherView.swift
+++ Sources/AnimatedLauncherUI/AnimatedLauncherView.swift
@@ -21,45 +21,46 @@ struct AnimatedLauncherView<Logo: View>: View {
     // MARK: - Body
     
     var body: some View {
-        Rectangle()
-            .fill(configLauncher.backgroundColor)
-            .mask {
-                GeometryReader { proxy in
-                    let size = proxy.size.applying(.init(scaleX: configLauncher.scaling, y: configLauncher.scaling))
-                    Rectangle()
-                        .overlay {
-                            logo
-                                .blur(radius: configLauncher.forceHideLogo ? .zero : (scaleUp ? 15 : .zero))
-                                .blendMode(.destinationOut)
-                                .animation(.smooth(duration: 0.3, extraBounce: .zero)) { content in
-                                    content
-                                        .scaleEffect(scaleDown ? 0.8 : 1)
-                                }
-                                .visualEffect { [scaleUp] content, proxy in
-                                    let scaleX: CGFloat = size.width / proxy.size.width
-                                    let scaleY: CGFloat = size.height / proxy.size.height
-                                    let maxScale = Swift.max(scaleX, scaleY)
-                                    return content.scaleEffect(scaleUp ? maxScale : 1)
-                                }
-                        }
-                }
+        ZStack {
+            Rectangle()
+                .fill(configLauncher.backgroundColor)
+                .ignoresSafeArea()
+            
+            GeometryReader { proxy in
+                let size = proxy.size.applying(.init(scaleX: configLauncher.scaling, y: configLauncher.scaling))
+                
+                logo
+                    .blur(radius: configLauncher.forceHideLogo ? .zero : (scaleUp ? 15 : .zero))
+                    .animation(.smooth(duration: 0.3, extraBounce: .zero)) { content in
+                        content
+                            .scaleEffect(scaleDown ? 0.8 : 1)
+                    }
+                    .visualEffect { [scaleUp] content, proxy in
+                        let scaleX: CGFloat = size.width / proxy.size.width
+                        let scaleY: CGFloat = size.height / proxy.size.height
+                        let maxScale = Swift.max(scaleX, scaleY)
+                        return content.scaleEffect(scaleUp ? maxScale : 1)
+                    }
+                    .opacity(configLauncher.forceHideLogo ? 0 : 1)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
-            .opacity(configLauncher.forceHideLogo ? 1 : (scaleUp ?  .zero : 1))
-            .background {
-                Rectangle()
-                    .fill(configLauncher.logoBackground)
-                    .opacity(scaleUp ? .zero : 1)
-            }
-            .ignoresSafeArea()
-            .task {
-                guard scaleDown.not else { return }
-                try? await Task.sleep(for: .seconds(configLauncher.initialDelay))
-                scaleDown = true
-                try? await Task.sleep(for: .seconds(0.1))
-                withAnimation(configLauncher.animation, completionCriteria: .logicallyComplete, {
-                    scaleUp = true
-                }, completion: isCompleted)
-            }
+        }
+        .opacity(configLauncher.forceHideLogo ? 1 : (scaleUp ?  .zero : 1))
+        .background {
+            Rectangle()
+                .fill(configLauncher.logoBackground)
+                .opacity(scaleUp ? .zero : 1)
+        }
+        .ignoresSafeArea()
+        .task {
+            guard scaleDown.not else { return }
+            try? await Task.sleep(for: .seconds(configLauncher.initialDelay))
+            scaleDown = true
+            try? await Task.sleep(for: .seconds(0.1))
+            withAnimation(configLauncher.animation, completionCriteria: .logicallyComplete, {
+                scaleUp = true
+            }, completion: isCompleted)
+        }
     }
 }
 
